196c441197143c8b382a5314047ce21c591f83dc
Paul Thomson <paulthomson@google.com>
2019-04-01 17:30:48 +0100

    reduce: remove module instruction pass

--- source/reduce/reducer.cpp
+++ source/reduce/reducer.cpp
@@ -20,13 +20,14 @@
 #include "source/reduce/operand_to_const_reduction_opportunity_finder.h"
 #include "source/reduce/operand_to_dominating_id_reduction_opportunity_finder.h"
 #include "source/reduce/operand_to_undef_reduction_opportunity_finder.h"
+#include "source/reduce/remove_block_reduction_opportunity_finder.h"
 #include "source/reduce/remove_function_reduction_opportunity_finder.h"
+#include "source/reduce/remove_module_instruction_reduction_opportunity_finder.h"
 #include "source/reduce/remove_opname_instruction_reduction_opportunity_finder.h"
 #include "source/reduce/remove_selection_reduction_opportunity_finder.h"
 #include "source/reduce/remove_unreferenced_instruction_reduction_opportunity_finder.h"
 #include "source/reduce/simple_conditional_branch_to_branch_opportunity_finder.h"
 #include "source/reduce/structured_loop_to_selection_reduction_opportunity_finder.h"
-#include "source/reduce/remove_block_reduction_opportunity_finder.h"
 #include "source/spirv_reducer_options.h"
 
 #include "reducer.h"
@@ -198,6 +199,8 @@ void Reducer::AddDefaultReductionPasses() {
           ConditionalBranchToSimpleConditionalBranchOpportunityFinder>());
   AddReductionPass(
       spvtools::MakeUnique<SimpleConditionalBranchToBranchOpportunityFinder>());
+  AddReductionPass(spvtools::MakeUnique<
+                   RemoveModuleInstructionReductionOpportunityFinder>());
 }
 
 void Reducer::AddReductionPass(
